614781250e9675fbda9ee899c3ed662e408c39aa
Kabucho G <kabuchogeorge3@gmail.com>
2023-03-23 16:04:17 -0700

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,38 @@
 #!/usr/bin/python3
+# def pascal_triangle(n):
+#     if n <= 0:
+#         return []
+
+#     triangle = [[1]]
+
+#     for i in range(1, n):
+#         row = [1]
+#         for j in range(1, i):
+#             row.append(triangle[i-1][j-1] + triangle[i-1][j])
+#         row.append(1)
+#         triangle.append(row)
+
+#     return triangle
+""" Module for calculating Pascal Triangle """
+
+
 def pascal_triangle(n):
+    """ Function for creating a pascal triangle as a list of lists
+    n: number of rows
+    returns empty list if n <= 0
+    """
     if n <= 0:
-        return []
+        return ([])
 
-    triangle = [[1]]
-
+    pascal = [[1]]
     for i in range(1, n):
         row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
+        prev = pascal[i - 1]
+        for j in range(len(prev)):
+            new = prev[j] + prev[j + 1] if j != len(prev) - 1 else 1
+            row.append(new)
 
-    return triangle
+        pascal.append(row)
+
+    return pascal
+
